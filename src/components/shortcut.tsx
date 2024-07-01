5fa4edf98a4039f1f02a3055d56ff2dde4e30431
TyyyHo <ty.ho1126@gmail.com>
2024-07-01 14:04:03 +0800

    add: icon. update: path of image.

--- src/components/shortcut.tsx
+++ src/components/shortcut.tsx
@@ -4,10 +4,10 @@ export default function Shortcut(): ReactNode {
   return (
     <section id="shortcut">
       <button id="house">
-        <img src="/icon/house.webp" alt="house" />
+        <img src="/floating-island/public/icon/house.webp" alt="house" />
       </button>
       <button id="basement">
-        <img src="/icon/basement.webp" alt="basement" />
+        <img src="/floating-island/public/icon/basement.webp" alt="basement" />
       </button>
     </section>
   );
